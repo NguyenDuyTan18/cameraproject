02f51c37ca8044dab6f953f149c404e9659f8fbd
NguyenDuyTan18 <nguyenduytanst2020@gmail.com>
2024-09-28 23:12:51 +0700

    success main-introduce commit

--- main.js
+++ main.js
@@ -14,13 +14,31 @@
         }
     } 
     setInterval(changeImage,5000)
-    // banner 
-    
+// banner 
+// main
     // main--introduce
-    const prev = document.querySelector('.main-introduce--prev');
-    const next = document.querySelector('.main-introduce--next');
-    const container = document.querySelector('.main-introduce__container');
+    const container = document.querySelector('.main-introduce__container-one');
+    const container_two = document.querySelector('.main-introduce__container-two');
+    const hotdealbutton = document.querySelector('.main-introduce__button--one');
+    const newbutton = document.querySelector(".main-introduce__button--two")
+    const introduce_onebuttom = document.querySelector(".main-introduce__list--content-one");
+    const introduce_twobuttom = document.querySelector(".main-introduce__list--content-two")
+
+    // làm xuất hiện hotdeal hoặc sắp ra mắt
+    function showHotdeal(){
+        introduce_onebuttom.classList.add('open');
+        introduce_twobuttom.classList.remove("open")
+    }
+    function showNew(){
+        introduce_twobuttom.classList.add('open');
+        introduce_onebuttom.classList.remove("open")
+    }
     
+    hotdealbutton.addEventListener('click', showHotdeal);
+    newbutton.addEventListener('click', showNew);
+    
+    // làm xuất hiện hotdeal hoặc sắp ra mắt
+    // trượt của hotdeal
     let index__introduce = 0;
     
     function showSlide(index){
@@ -43,11 +61,39 @@
     function moveSlide(step){
         showSlide(index__introduce + step)
     }
-    
-    
-    
-    // main--introduce
+    // trượt của hotdeal
 
+
+    // trượt của sắp ra mắt
+    let index__introduce_two = 0;
+    
+    function showSlide_two(index){
+        const totalcontainer_two = container_two.children.length;
+        console.log(totalcontainer_two)
+        if(index >= totalcontainer_two  ){
+            index__introduce_two = 0;
+        }
+        else if(index < 0){
+            index__introduce_two = totalcontainer_two - 1;
+        }
+        else {
+            index__introduce_two = index;
+        }
+        console.log(index__introduce_two)
+        const containerWidth_two = container_two.children[0].clientWidth
+        console.log(containerWidth_two);
+        container_two.style.transform = `translateX(${-index__introduce_two * containerWidth_two}px)`;
+    }
+    function moveSlide_two(step){
+        showSlide_two(index__introduce_two + step)
+    }
+
+
+    // trượt của sắp ra mắt
+
+
+    // main--introduce
+    // main
 
     
 
